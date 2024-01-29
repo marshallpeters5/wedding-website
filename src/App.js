6822ee943a0378a523152f1d6d96a9012c67ee3b
Marshall <marshall.peters01@gmail.com>
2024-01-29 16:42:27 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Home from './Components/Home';
+import OurStory from './Components/OurStory';
+import PhotoAlbum from './Components/PhotoAlbum';
+import WeddingParty from './Components/WeddingParty';
+import Honeymoon from './Components/Honeymoon';
+import TravelInformation from './Components/TravelInformation';
+import Registry from './Components/Registry';
+import FAQ from './Components/FAQ';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/our-story">Our Story</Link>
+        <Link to="/photo-album">Photo Album</Link>
+        <Link to="/wedding-party">Wedding Party</Link>
+        <Link to="/honeymoon">Honeymoon</Link>
+        <Link to="/travel-information">Travel Information</Link>
+        <Link to="/registry">Registry</Link>
+        <Link to="/faq">FAQ</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/our-story" element={<OurStory />} />
+        <Route path="/photo-album" element={<PhotoAlbum />} />
+        <Route path="/wedding-party" element={<WeddingParty />} />
+        <Route path="/honeymoon" element={<Honeymoon />} />
+        <Route path="/travel-information" element={<TravelInformation />} />
+        <Route path="/registry" element={<Registry />} />
+        <Route path="/faq" element={<FAQ />} />
+      </Routes>
+    </Router>
   );
 }
 
